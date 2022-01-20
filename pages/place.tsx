ba9942cd70c31f1a09ae592bad0542895795cd36
ana scuturici <anascuturici@gmail.com>
2022-01-20 22:29:37 +0900

    add: update UI elements

--- pages/place.tsx
+++ pages/place.tsx
@@ -1,37 +1,39 @@
 import Link from "next/link";
-import { Container, Button, Stack, ButtonGroup, Box } from '@chakra-ui/react'
+import { Center, Stack, Button, ButtonGroup, Box } from '@chakra-ui/react'
 import { atom, selector, useRecoilState } from 'recoil'
+import { URL } from "url";
 
 export default function place() {
   // const [ place, setPlace ] = useRecoilState()
   // const [ businessHours, setBusinessHours ] = useRecoilState()
-  const backgroundPhoto = "url(https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.theworlds50best.com%2Fdiscovery%2FEstablishments%2FJapan%2FTokyo%2FStar-Bar.html&psig=AOvVaw21dFIFNLitlf2EonvIzjj5&ust=1642749951263000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKjKgdPmv_UCFQAAAAAdAAAAABAD"
+  const backgroundPhoto = 'https://www.theworlds50best.com/discovery/filestore/jpg/StarBar-Tokyo-Japan-01.jpg'
 
   return (
     <>
-      <Container maxW='sm' borderWidth='1px' borderRadius='lg' align='center'>
-      <div>WELCOME TO PLACE</div>
-        <Stack direction='column' spacing={4} align='center' mt={70} mb={200}>
-          <Box bg='grey.300' borderWidth='1px' w='50%' p={4} color='grey.700' align='center' bgImage={backgroundPhoto}>
-            Place name
-          </Box>
-          <Box bg='green.100' w='30%' p={4} color='grey.700' align='center'>
-            Business hours
-          </Box>
+      <Center h='100vh' bg='teal.500'>
+        <Stack h='95vh' boxShadow='md' bg='whiteAlpha.900' p='20' rounded='md' backgroundImage={`url(${backgroundPhoto})`}>
+          <Stack direction='column' spacing={4} align='center' pb={250}>
+            <Box bg='green.100' borderWidth='1px' w='50%' p={4} align='center'>
+              Place name
+            </Box>
+            <Box bg='green.100' w='40%' p={4} color='grey.700' align='center'>
+              Business hours
+            </Box>
+          </Stack>
+          <ButtonGroup direction='row' spacing={4} align='center'>
+            <Link href='/selection'>
+              <Button bg='green.100'>
+                Go to Selection
+              </Button>
+            </Link>
+            <Link href='/navigation'>
+              <Button bg='green.100'>
+                Go to Navigation
+              </Button>
+            </Link>
+          </ButtonGroup>
         </Stack>
-        <ButtonGroup direction='row' spacing={4} align='center' mb={50}>
-          <Link href='/selection'>
-            <Button>
-              Go to Selection
-            </Button>
-          </Link>
-          <Link href='/navigation'>
-            <Button>
-              Go to Navigation
-            </Button>
-          </Link>
-        </ButtonGroup>
-      </Container>
+      </Center>
     </>
   );
 }
