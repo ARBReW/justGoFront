56b1952ffb98bad54b1d714b9e4b97b3fb25d229
Wesley <leewes5928@users.noreply.github.com>
2022-01-20 20:23:35 +0900

    refactor: images change using recoil

--- pages/selection.tsx
+++ pages/selection.tsx
@@ -1,31 +1,20 @@
 import { Box, Button, Center, HStack, Stack } from "@chakra-ui/react";
 import Link from "next/link";
 import { useState } from "react";
+import { useRecoilValue } from "recoil";
+import locationStates, { routes } from "../states/locationStates";
 
 export default function selection() {
-  const images: string[] = [
-    "https://parks.pflugervilletx.gov/home/showpublishedimage/15033/637641079093830000",
-    "noimage",
-    "noimage",
-    "noimage",
-    "https://media-cdn.tripadvisor.com/media/photo-s/12/92/53/4a/1f.jpg",
-  ];
+  const routesList = useRecoilValue(routes);
+  const { places } = useRecoilValue(locationStates);
 
-  const names: string[] = [
-    "Richard Park",
-    "Brian Bar",
-    "Ana's Place",
-    "Wesley Store",
-    "Roman Mall",
-  ];
-
-  const placeData = names.map((name: string, i) => [name, images[i]]);
-
-  const [bg, setBg] = useState(placeData[0][1]);
+  const [selectRoute, setSelectRoute] = useState(0);
+  const [bg, setBg] = useState("");
 
   function changeRoute(event: any) {
     const direction: string = event.target.attributes.direction.value;
-    console.log(direction);
+    if (direction === "left") {
+    }
   }
 
   function handleSelection() {
@@ -33,11 +22,12 @@ export default function selection() {
   }
 
   function handleEnd() {
-    console.log("We are done");
+    console.log(routesList);
   }
 
   function changeBg(event: any) {
-    setBg(event.target.attributes.img.value);
+    const placeId = Number.parseInt(event.target.attributes.placeid.value);
+    setBg(places.find(place => place.placeId === placeId)!.img)
   }
 
   return (
@@ -50,14 +40,20 @@ export default function selection() {
             </Button>
             <Center>
               <Stack>
-                <div>WELCOME TO SELECTION</div>
-                {placeData.reverse().map((place, i) => {
-                  return (
-                    <Button key={i * 3.14} img={place[1]} onClick={changeBg}>
-                      {place[0]}
-                    </Button>
-                  );
-                })}
+                {routesList[selectRoute]
+                  .slice()
+                  .reverse()
+                  .map((place) => {
+                    return (
+                      <Button
+                        key={place?.placeId}
+                        placeid={place?.placeId}
+                        onClick={changeBg}
+                      >
+                        {place?.name}
+                      </Button>
+                    );
+                  })}
                 <Link href="/place" passHref>
                   <Button onClick={handleSelection}>JUST GO</Button>
                 </Link>
