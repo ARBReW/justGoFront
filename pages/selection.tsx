8b02cfd0e39978aaf03170d703efde2c1e9a4432
Wesley <leewes5928@users.noreply.github.com>
2022-01-20 16:12:40 +0900

    feat: display names of places in route

--- pages/selection.tsx
+++ pages/selection.tsx
@@ -1,11 +1,20 @@
 import { Box, Button, Center, HStack, Stack } from "@chakra-ui/react";
+import { map } from "lodash";
 import Link from "next/link";
 
 export default function selection() {
   const images: string[] = [
     "https://images.squarespace-cdn.com/content/v1/58fd82dbbf629ab224f81b68/1530854536400-U501LPJSRZ0W57S9SH4V/Shirahige-Shinto-Shrine.jpg?format=1000w",
   ];
-  
+
+  const names: string[] = [
+    "Richard Park",
+    "Brian Bar",
+    "Ana's Place",
+    "Wesley Store",
+    "Roman Mall",
+  ];
+
   function changeRoute(event: any) {
     const direction: string = event.target.attributes.direction.value;
     console.log(direction);
@@ -22,10 +31,7 @@ export default function selection() {
   return (
     <>
       <Center>
-        <Box
-          backgroundImage={images[0]}
-          backgroundRepeat="no-repeat"
-        >
+        <Box backgroundImage={images[0]} backgroundRepeat="no-repeat">
           <HStack>
             <Button direction="left" onClick={changeRoute}>
               Left
@@ -33,6 +39,9 @@ export default function selection() {
             <Center>
               <Stack>
                 <div>WELCOME TO SELECTION</div>
+                {names.map((name: string) => {
+                  return <div>{name}</div>;
+                })}
                 <Link href="/place">
                   <Button onClick={handleSelection}>JUST GO</Button>
                 </Link>
