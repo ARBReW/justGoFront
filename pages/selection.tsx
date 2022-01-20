3a38a1fd2c1d775c9a5ac131e07e90af00810672
Wesley <leewes5928@users.noreply.github.com>
2022-01-20 16:02:39 +0900

    feat: add handleSelection, changeRoute, and handleEnd methods

--- pages/selection.tsx
+++ pages/selection.tsx
@@ -1,24 +1,52 @@
-import { Button, Center, HStack, Stack } from "@chakra-ui/react";
+import { Box, Button, Center, HStack, Stack } from "@chakra-ui/react";
 import Link from "next/link";
 
 export default function selection() {
+  const images: string[] = [
+    "https://images.squarespace-cdn.com/content/v1/58fd82dbbf629ab224f81b68/1530854536400-U501LPJSRZ0W57S9SH4V/Shirahige-Shinto-Shrine.jpg?format=1000w",
+  ];
+  
+  function changeRoute(event: any) {
+    const direction: string = event.target.attributes.direction.value;
+    console.log(direction);
+  }
+
+  function handleSelection() {
+    console.log("selectedPlace");
+  }
+
+  function handleEnd() {
+    console.log("We are done");
+  }
+
   return (
     <>
-      <HStack>
-        <Button>Left</Button>
-        <Center>
-          <Stack>
-            <div>WELCOME TO SELECTION</div>
-            <Link href="/place">
-              <Button>Go To Place</Button>
-            </Link>
-            <Link href="/otsukare">
-              <Button>Go To Otsukare</Button>
-            </Link>
-          </Stack>
-        </Center>
-        <Button>Right</Button>
-      </HStack>
+      <Center>
+        <Box
+          backgroundImage={images[0]}
+          backgroundRepeat="no-repeat"
+        >
+          <HStack>
+            <Button direction="left" onClick={changeRoute}>
+              Left
+            </Button>
+            <Center>
+              <Stack>
+                <div>WELCOME TO SELECTION</div>
+                <Link href="/place">
+                  <Button onClick={handleSelection}>JUST GO</Button>
+                </Link>
+                <Link href="/otsukare">
+                  <Button onClick={handleEnd}>Go To Otsukare</Button>
+                </Link>
+              </Stack>
+            </Center>
+            <Button direction="right" onClick={changeRoute}>
+              Right
+            </Button>
+          </HStack>
+        </Box>
+      </Center>
     </>
   );
 }
