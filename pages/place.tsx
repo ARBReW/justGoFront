b0b1ed1ccbd0576baba146dcb9ee633bf363a3a0
ana scuturici <anascuturici@gmail.com>
2022-01-21 10:08:27 +0900

    update: refactor UI on place page

--- pages/place.tsx
+++ pages/place.tsx
@@ -1,11 +1,47 @@
 import Link from "next/link";
+import { Center, Stack, Button, ButtonGroup, Box } from '@chakra-ui/react';
+import { useRecoilValue } from "recoil";
+import locationStates from "../states/locationStates";
 
 export default function place() {
+  const { places } = useRecoilValue(locationStates);
+
+  console.log(places);
+
   return (
     <>
-      <div>WELCOME TO PLACE</div>
-      <Link href='/selection'>Go to Selection</Link>
-      <Link href='/navigation'>Go to Navigation</Link>
+      <Center h='100vh' bg='teal.500'>
+        <Stack 
+        h='95vh' 
+        boxShadow='md' 
+        bg='whiteAlpha.900' 
+        p='20' rounded='md' 
+        backgroundImage={`url(${places[0].img})`}
+        backgroundRepeat='no-repeat'
+        backgroundPosition='center'
+        backgroundSize='cover'>
+          <Stack direction='column' spacing={4} align='center' pb={250}>
+            <Box bg='green.100' borderWidth='1px' w='50%' p={4} align='center'>
+              {places[0].name}
+            </Box>
+            <Box bg='green.100' w='50%' p={4} color='grey.700' align='center'>
+              Open {places[0].hours.open} to {places[0].hours.close}
+            </Box>
+          </Stack>
+          <ButtonGroup direction='row' spacing={4} align='center'>
+            <Link href='/selection'>
+              <Button bg='green.100'>
+                Go to Selection
+              </Button>
+            </Link>
+            <Link href='/navigation'>
+              <Button bg='green.100'>
+                Go to Navigation
+              </Button>
+            </Link>
+          </ButtonGroup>
+        </Stack>
+      </Center>
     </>
   );
 }
