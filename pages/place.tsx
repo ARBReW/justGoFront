7e9e298de03967f407f808ccbfd1986d5658530b
ana scuturici <anascuturici@gmail.com>
2022-01-21 10:41:19 +0900

    refactor: refactor place UI

--- pages/place.tsx
+++ pages/place.tsx
@@ -11,24 +11,42 @@ export default function place() {
   return (
     <>
       <Center h='100vh' bg='teal.500'>
-        <Stack 
-        h='95vh' 
-        boxShadow='md' 
-        bg='whiteAlpha.900' 
-        p='20' rounded='md' 
-        backgroundImage={`url(${places[0].img})`}
-        backgroundRepeat='no-repeat'
-        backgroundPosition='center'
-        backgroundSize='cover'>
-          <Stack direction='column' spacing={4} align='center' pb={250}>
-            <Box bg='green.100' borderWidth='1px' w='50%' p={4} align='center'>
+        <Stack
+          h='95vh'
+          boxShadow='md'
+          bg='whiteAlpha.900'
+          p='20' rounded='md'
+          backgroundImage={`url(${places[0].img})`}
+          backgroundRepeat='no-repeat'
+          backgroundPosition='center'
+          backgroundSize='cover'>
+          <Stack
+            direction='column'
+            spacing={4}
+            align='center'>
+            <Box
+              bg='green.100'
+              borderWidth='1px'
+              w='50%'
+              p={4}
+              align='center'>
               {places[0].name}
             </Box>
-            <Box bg='green.100' w='50%' p={4} color='grey.700' align='center'>
+            <Box
+              bg='green.100'
+              w='50%'
+              p={4}
+              color='grey.700'
+              align='center'>
               Open {places[0].hours.open} to {places[0].hours.close}
             </Box>
           </Stack>
-          <ButtonGroup direction='row' spacing={4} align='center'>
+          <ButtonGroup
+            direction='row'
+            spacing={4}
+            align='center'
+            pt={250}
+            pb={50}>
             <Link href='/selection'>
               <Button bg='green.100'>
                 Go to Selection
