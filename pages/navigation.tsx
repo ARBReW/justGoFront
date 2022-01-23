94764125119da4db8e7f0cbd3a725ee49a6c9dc4
richardpb <rpbeavis@gmail.com>
2022-01-23 16:50:37 +0900

    fix: add condition to not update next place details when at final location

--- pages/navigation.tsx
+++ pages/navigation.tsx
@@ -12,11 +12,16 @@ export default function navigation() {
   const [traveledRoute, setTraveledRoute] = useRecoilState<userRouteInterface>(userRoute);
 
   const nextPlace = () => {
-    setPlaceInfo(currRoute.stops[currRoute.stops.indexOf(places) + 1]);
+    let placeIndex = currRoute.stops.indexOf(places) + 1;
+    if (placeIndex > currRoute.stops.length - 1) {
+      setPlaceInfo(currRoute.stops[placeIndex - 1]);
+    } else {
+      setPlaceInfo(currRoute.stops[placeIndex]);
+    }
   }
 
   const updateUserRoute = () => {
-    setTraveledRoute({...traveledRoute, completedRoute:[...traveledRoute.completedRoute, placeInfo]});
+    setTraveledRoute({ ...traveledRoute, completedRoute: [...traveledRoute.completedRoute, placeInfo] });
     nextPlace();
   }
   return (
