efc90675f9c8501b151044bd888d752c1adc99cb
Brian Lee <ilovenocchi@gmail.com>
2022-01-23 13:49:47 +0900

    fix: login CSS (midway), delete default CSS files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import {RecoilRoot} from 'recoil'  
 import { ChakraProvider } from "@chakra-ui/react";
 import { extendTheme } from "@chakra-ui/react";
+import { createBreakpoints } from "@chakra-ui/theme-tools";
+
+const breakpoints = createBreakpoints({
+  sm: "30em",
+  md: "48em",
+  lg: "62em",
+  xl: "80em",
+  "2xl": "96em",
+});
 
 const colors = {
   brand: {
@@ -12,7 +20,7 @@ const colors = {
   },
 };
 
-const theme = extendTheme({ colors });
+const theme = extendTheme({ colors, breakpoints });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
