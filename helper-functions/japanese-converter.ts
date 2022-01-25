10f9972ef406b29fa6774c7e4f54c06730db76b7
ana scuturici <anascuturici@gmail.com>
2022-01-26 08:51:08 +0900

    add: testing japanese conversion libraries

---
+++ helper-functions/japanese-converter.ts
@@ -0,0 +1,24 @@
+//import * as wanakana from 'wanakana';
+// import { isJapanese, toKana, toRomaji } from 'wanakana';
+
+// const convertToRomaji = (string: string) => {
+//     const splitStr = string.split(' ');
+//     for(let str of splitStr){
+//         if(isJapanese(str)){
+//             return toKana(str);
+//            return string.replace(/str/g, toRomaji(str));
+//         } 
+//     }
+// }
+
+// import Kuroshiro from "kuroshiro";
+// // import KuromojiAnalyzer from "kuroshiro-analyzer-kuromoji";
+// // const kuroshiro = new Kuroshiro();
+
+// const convertToRomaji = async () => {
+//     await kuroshiro.init(new KuromojiAnalyzer());
+//     const result = await kuroshiro.convert("感じ取れたら手を繋ごう、重なるのは人生のライン and レミリア最高！", { to: "hiragana" });
+//     const result2 = Kuroshiro.Util.isHiragana("あ");
+// }       
+
+// export default convertToRomaji;
