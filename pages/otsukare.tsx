d090f7dfe31ea37604c262977f687294339bf594
ana scuturici <anascuturici@gmail.com>
2022-02-04 19:57:41 +0900

    refactor: otsukare page css

--- pages/otsukare.tsx
+++ pages/otsukare.tsx
@@ -6,6 +6,7 @@ import {
   Image,
   AspectRatio,
   HStack,
+  Box
 } from "@chakra-ui/react";
 import currentRoute from "../states/currentRoute";
 import { useRecoilValue, useResetRecoilState } from "recoil";
@@ -30,58 +31,63 @@ export default function showRoute() {
   return (
     <>
       <Stack h="95vh" align="center">
-        <Heading
-          pt="20"
+        <Text
+          pt="15"
           justifyContent="center"
-          fontSize={["5vh", "5vh", "5vh", "5vh"]}
+          color="whitesmoke"
           fontFamily={"body"}
-          fontWeight="bold"
-          color="tomato"
+          m="5"
+          fontSize={25}
+          fontWeight={"bold"}
+          textShadow={"-1.1px -1.1px #52796F, -1px 1.1px #52796F, 1px -1.1px #52796F, 1px 1.1px #52796F"}
         >
-          🎊Otsukare 🎊
-        </Heading>
+          Otsukare 
+        </Text>
 
-        <AspectRatio pt="5" minW="300px" maxW="70%" maxH="50vh" ratio={4 / 3}>
+        <AspectRatio pt="1" minW="300px" maxW="70%" maxH="50vh" ratio={4 / 3}>
           <Image
             src={endImg}
             rounded="lg"
             objectFit="cover"
             objectPosition="50%"
+            border="2px solid white"
+            boxShadow="dark-lg"
+            p="2"
+            bg="whiteAlpha.400"
           />
         </AspectRatio>
 
-        <Stack pt={10} spacing={2} align={"center"}>
+        <Stack pt={3} spacing={2} align={"center"}>
           <Text
             fontSize={["2.5vh", "2.5vh", "2.5vh", "2.5vh"]}
             textTransform={"uppercase"}
+            fontWeight="bold"
+            color="whitesmoke"
+            textShadow={"-1.1px -1.1px #52796F, -1px 1.1px #52796F, 1px -1.1px #52796F, 1px 1.1px #52796F"}
+            m="5"
           >
             Your route:
           </Text>
         </Stack>
 
-        <div>
+        <Box
+        border="1px solid white"
+        rounded="lg"
+        boxShadow="dark-lg"
+        p="2"
+        bg="whiteAlpha.200">
           {completedRoute
             .slice()
             .reverse()
             .map((stop) => (
-              <HStack bg="whiteAlpha.900" key={stop._id + "CC24 rocks"} w="100%" spacing="0">
-               <Text> {"✅ "} </Text>  <Image h="2vh" src={stop?.type} pr="3px"></Image> <Text>{stop?.name}</Text>
+              <HStack bg="whiteAlpha.900" key={stop._id + "CC24 rocks"} w="100%" spacing="0.5" border="1px" rounded="sm">
+               <Text> {"✅ "} </Text>  
+               <Image h="2vh" src={stop?.type} pr="3px"></Image> 
+               <Text>{stop?.name}</Text>
               </HStack>
             ))}
-        </div>
+        </Box>
 
-        <Stack pt={10} align={"center"}>
-          <Link href="/" passHref>
-            <Button
-              colorScheme="blackAlpha"
-              variant="solid"
-              onClick={clearUser}
-              fontSize={["2.2vh", "2.2vh", "2.2vh", "2.2vh"]}
-            >
-              Return to login
-            </Button>
-          </Link>
-        </Stack>
       </Stack>
     </>
   );
